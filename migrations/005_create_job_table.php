40f22993d9b8e96af7e41c00d1183081127a449b
Rizky phs <rizkyputrahadisarwono@gmail.com>
2025-08-25 08:39:12 +0700

    migrasi wf

--- migrations/005_create_job_table.php
+++ migrations/005_create_job_table.php
@@ -1,14 +1,25 @@
 <?php
+
 use PDO;
 
 return [
     'up' => function (PDO $pdo) {
-        $sql = "CREATE TABLE";
+        $sql = "CREATE TABLE work_force(
+            workforce_id INT AUTO_INCREMENT PRIMARY KEY,
+            employee_id INT NOT NULL,
+            workforce_name VARCHAR(100) NOT NULL,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+            -- Relasi ke master_employees
+            CONSTRAINT fk_employee_workforce FOREIGN KEY (employee_id) 
+                REFERENCES employees(employee_id)
+                ON DELETE CASCADE
+                ON UPDATE CASCADE
+            )
+        ";
         $pdo->exec($sql);
     },
-    'down' => function (PDO $pdo) {
-        
-    }
+    'down' => function (PDO $pdo) {}
 ];
-
 ?>
