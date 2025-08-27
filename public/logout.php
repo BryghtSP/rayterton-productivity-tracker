65e42a987467f7b72665f4eed5fd6f2a08f0d960
Rizky phs <rizkyputrahadisarwono@gmail.com>
2025-08-27 23:26:20 +0700

    upload

--- public/logout.php
+++ public/logout.php
@@ -1,4 +1,4 @@
 <?php
 require_once __DIR__ . '/../lib/auth.php';
 session_destroy();
-header("Location: http://localhost/rayterton-productivity-tracker/public/");
+header("Location: https://raytertonapps.com/prodtracker/public/");
