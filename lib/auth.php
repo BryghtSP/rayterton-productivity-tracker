d30e369d9cdd345fd63dba16a7811e84acd0978a
BryghtSP <bsyahputra.2008@gmail.com>
2025-09-03 13:46:22 +0700

    Fix Session Commit

--- lib/auth.php
+++ lib/auth.php
@@ -1,10 +1,12 @@
-<?php
+ <?php
 // lib/auth.php
 session_start();
+require_once __DIR__ . '/config.php';
 
 function require_login() {
+  global $BASE_URL;
   if (!isset($_SESSION['user'])) {
-    header("Location: /index.php");
+    header("Location: {$BASE_URL}/index.php");
     exit;
   }
 }
@@ -17,4 +19,4 @@ function require_admin() {
     exit;
   }
 }
-?>
+
